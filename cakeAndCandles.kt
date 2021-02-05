4e5f9b5098f85ec9421818168f29dfcf4bac8ea8
codingfanatic <lclarkerichard@yahoo.com>
2021-02-04 20:29:23 -0500

    Worked on Cake and Candles problem Added variables for age and layers Created empty methods for printing cake candles, top, and bottom Finished the printCakeTop method

--- cakeAndCandles.kt
+++ cakeAndCandles.kt
@@ -1,9 +1,34 @@
 fun main(){
+    //1. Create a variable age and set it to 24
+    //2. Create a variable layers set to 5
+    //3. In main(), call a function printCakeCandles() and pass age
+    //4. In main(), call a function printCakeTop and also pass in the age
+    //5. In main(), call a function printCakeBottom and pass in the age and number of layers
     val age = 24
     val layers = 5
-    printCakeCAndles(age)
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
 }
 
 fun printCakeCandles(age: Int){
     
+}
+
+fun printCakeTop(age: Int){
+    /*
+    Use a repeat() statement to print one equal sign age 
+    times plus 2. The extra two equals signs are so that 
+    the candles won't fall off the side of the cake.
+    At the end, when the repeat statement is done, print an
+    empty line.
+    */
+    repeat(age + 2){
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(age: Int, layers: Int){
+
 }
